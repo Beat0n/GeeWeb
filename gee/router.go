ed5809f72fd10e1936cde297aca73be0bb007d6b
胡冀哲 <hujizheuestc@163.com>
2022-09-24 17:00:15 +0800

    ""

--- gee/router.go
+++ gee/router.go
@@ -1,9 +1,17 @@
 package gee
 
+import "log"
+
 type router struct {
 	handlers map[string]HandlerFunc
 }
 
 func newRouter() *router {
 	return &router{make(map[string]HandlerFunc)}
+}
+
+func (r *router) addRoute(method, pattern string, handler HandlerFunc) {
+	log.Printf("Route %4s - %s", method, pattern)
+	key := method + "-" + pattern
+	r.handlers[key] = handler
 }
