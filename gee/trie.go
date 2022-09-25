291cb75dbb686781ec0f8f915308b45cfad1c08b
Beat0n <hujizheuestc@163.com>
2022-09-25 21:24:02 +0800

    ""

--- gee/trie.go
+++ gee/trie.go
@@ -30,21 +30,6 @@ func (n *node) matchChildren(part string) []*node {
 	return nodes
 }
 
-func (n *node) insert(pattern string, parts []string, height int) {
-	if len(parts) == height {
-		n.pattern = pattern
-		return
-	}
-
-	part := parts[height]
-	child := n.matchChild(part)
-	if child == nil {
-		child = &node{part: part, isWild: part[0] == ':' || part[0] == '*'}
-		n.children = append(n.children, child)
-	}
-	child.insert(pattern, parts, height+1)
-}
-
 func (n *node) search(parts []string, height int) *node {
 	if len(parts) == height || strings.HasPrefix(n.part, "*") {
 		if n.pattern == "" {
@@ -62,6 +47,22 @@ func (n *node) search(parts []string, height int) *node {
 			return result
 		}
 	}
-
 	return nil
+}
+
+func (n *node) insert(pattern string, parts []string, height int) {
+	if len(parts) == height {
+		n.pattern = pattern
+		return
+	}
+	part := parts[height]
+	child := n.matchChild(part)
+	if child == nil {
+		child := &node{
+			part:   part,
+			isWild: part[0] == ':' || part[0] == '*',
+		}
+		n.children = append(n.children, child)
+	}
+	child.insert(pattern, parts, height+1)
 }
